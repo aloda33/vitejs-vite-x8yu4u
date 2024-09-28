88ec9fb1d9f01ab0e978ca9990280255101fc3e6
aloda33 <git@stackblitz.com>
2024-09-28 17:49:11 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react’;
+
+function Fetchdata() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(error => console.error(error));
+}, []); 
+
+return (
+  <div>
+  {data ? (
+  <ul>
+  {data.map(item => (
+  <li
+  key={item.id}>{item.name}</li>
+  ))}
+  </ul>
+) : (
+<p>Loading...</p>
+)}
+</div>
+);
+}
+
+export default Fetchdata;
